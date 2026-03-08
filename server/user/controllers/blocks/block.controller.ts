46eaf69b9ef45a15865a1acda2f8a6aa16f4caf5
Rohan <rsrohan787@gmail.com>
2026-03-08 12:25:08 +0530

    fixed

--- server/user/controllers/blocks/block.controller.ts
+++ server/user/controllers/blocks/block.controller.ts
@@ -6,7 +6,9 @@ export default class BlockController {
         try {
             const { Ulb_Id } = req.body;
 
-            const blocks: any = await BlockService.listBlocksByUlbId(Ulb_Id);
+            const {Block_Id} = req.body
+
+            const blocks: any = await BlockService.listBlocksByUlbId(Block_Id);
 
             if (!blocks) {
                 return res.send({
